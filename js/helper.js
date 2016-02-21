73e9ae3c9db1f832800481785b5d892308718005
Yi Wang <wangyi0101@gmail.com>
2016-02-21 10:59:40 -0800

    personalized layout of resume project. Reorganized directory.

--- js/helper.js
+++ js/helper.js
@@ -13,21 +13,21 @@ These are HTML strings. As part of the course, you'll be using JavaScript functi
 replace the %data% placeholder text you see in them.
 */
 var HTMLheaderName = '<h1 id="name">%data%</h1>';
-var HTMLheaderRole = '<span>%data%</span><hr>';
+var HTMLheaderRole = '<span class="orange-text">%data%</span><hr>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
-var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile</span><span class="white-text">%data%</span></li>';
-var HTMLemail = '<li class="flex-item"><span class="orange-text">email</span><span class="white-text">%data%</span></li>';
+var HTMLmobile = '<li class="fa fa-phone flex-item"><span class="white-text">%data%</span></li>';
+var HTMLemail = '<li class="fa fa-envelope flex-item"><span class="white-text">%data%</span></li>';
 var HTMLtwitter = '<li class="flex-item"><span class="orange-text">twitter</span><span class="white-text">%data%</span></li>';
-var HTMLgithub = '<li class="flex-item"><span class="orange-text">github</span><span class="white-text">%data%</span></li>';
+var HTMLgithub = '<li class="flex-item fa fa-github"><span class="white-text">%data%</span></li>';
 var HTMLblog = '<li class="flex-item"><span class="orange-text">blog</span><span class="white-text">%data%</span></li>';
-var HTMLlocation = '<li class="flex-item"><span class="orange-text">location</span><span class="white-text">%data%</span></li>';
+var HTMLlocation = '<li class="fa fa-location-arrow flex-item"><span class="white-text">%data%</span></li>';
 
 var HTMLbioPic = '<img src="%data%" class="biopic">';
 var HTMLwelcomeMsg = '<span class="welcome-message">%data%</span>';
 
-var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>';
-var HTMLskills = '<li class="flex-item"><span class="white-text">%data%</span></li>';
+var HTMLskillsStart = '<h3 id="skills-h3">Skills at a Glance:</h3>';
+var HTMLskills = '<li class="flex-item"><span class="skills">%data%</span></li>';
 
 var HTMLworkStart = '<div class="work-entry"></div>';
 var HTMLworkEmployer = '<a href="#">%data%';
@@ -37,20 +37,22 @@ var HTMLworkLocation = '<div class="location-text">%data%</div>';
 var HTMLworkDescription = '<p><br>%data%</p>';
 
 var HTMLprojectStart = '<div class="project-entry"></div>';
-var HTMLprojectTitle = '<a href="#">%data%</a>';
+var HTMLprojectTitle = '<div class="project_title"><a href="#">%data%</a></div>';
 var HTMLprojectDates = '<div class="date-text">%data%</div>';
 var HTMLprojectDescription = '<p><br>%data%</p>';
 var HTMLprojectImage = '<img src="%data%">';
+var HTMLprojectProgress = '<div class="progress"><div class="progress-bar progress-bar-success progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="#">%data%</div></div>';
+
 
 var HTMLschoolStart = '<div class="education-entry"></div>';
 var HTMLschoolName = '<a href="#">%data%';
 var HTMLschoolDegree = ' -- %data%</a>';
 var HTMLschoolDates = '<div class="date-text">%data%</div>';
 var HTMLschoolLocation = '<div class="location-text">%data%</div>';
-var HTMLschoolMajor = '<em><br>Major: %data%</em>';
+var HTMLschoolMajor = '<em><br><i class="fa fa-graduation-cap"></i> %data%</em>';
 
-var HTMLonlineClasses = '<h3>Online Classes</h3>';
-var HTMLonlineTitle = '<a href="#">%data%';
+var HTMLonlineClasses = '<h2><i class="fa fa-book"></i> Online Classes</h2>';
+var HTMLonlineTitle = '<a href="#"><i class="fa fa-graduation-cap"></i> %data%';
 var HTMLonlineSchool = ' - %data%</a>';
 var HTMLonlineDates = '<div class="date-text">%data%</div>';
 var HTMLonlineURL = '<br><a href="#">%data%</a>';
--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -8,9 +8,9 @@ var bio = {
         "github":"https://github.com/wangyi0101",
         "location":"Fremont,CA"
     },
-    "welcomeMessage": "I am a front-end Apprentice, who spend more than 10 hours each day to polish HTML5, CSS3 and JavaScript skills.I am eager to become a professional front-end developer, since I love Web. Web for me has infinite possibilities. I enjoy work that challenges me to explore new technical skills and touch the next level of achievement.",
-    "bioPic": "images/197x148.gif",
-    "skills": ["HTML5", "CSS3", "JavaScript", "JQuery", "Git", "Bash ", "Bootstrap", "Responsive Design", "Python", "SQL"]
+    "welcomeMessage": "I'm a front-end Apprentice, who spend more than 10 hours each day to polish HTML5, CSS3 and JavaScript skills. I am eager to become a professional web developer, since web for me has infinite possibilities. I enjoy work that challenges me to explore new technical skills and touch the next level of achievement. ",
+    "bioPic": "http://placehold.it/250x200",
+    "skills": ["HTML5, CSS3, JavaScript, JQuery, Git, Bash, Bootstrap, Responsive Design, Python, SQL"]
 }
 
 /* This functon is used to update name, role, contact information, bio image, welcome message and skills to the resume */
@@ -21,6 +21,18 @@ bio.display = function() {
     var formattedHeaderName = HTMLheaderName.replace("%data%", bio.name);
     $("#header").prepend(formattedHeaderName);
 
+     var formattedBiopic = HTMLbioPic.replace("%data%", bio.bioPic);
+    $("#header").append(formattedBiopic);
+
+    var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+    $("#header").append(formattedWelcome);
+
+    if(bio.skills.length > 0) {
+        $("#header").append(HTMLskillsStart);
+        var formattedSkills =HTMLskills.replace("%data%", bio.skills);
+        $("#header").append(formattedSkills);
+    }
+       
     var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
     $("#topContacts").append(formattedMobile);
     $("#footerContacts").append(formattedMobile);
@@ -37,23 +49,11 @@ bio.display = function() {
     $("#topContacts").append(formattedLocation);
     $("#footerContacts").append(formattedLocation);
 
-    var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
-    $("#topContacts").append(formattedTwitter);
-    $("#footerContacts").append(formattedTwitter);
+    // var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+    // $("#topContacts").append(formattedTwitter);
+    // $("#footerContacts").append(formattedTwitter);
 
-    var formattedBiopic = HTMLbioPic.replace("%data%", bio.bioPic);
-    $("#header").append(formattedBiopic);
-
-    var formattedWelcome = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
-    $("#header").append(formattedWelcome);
-
-    if(bio.skills.length > 0) {
-        $("#header").append(HTMLskillsStart);
-
-        var formattedSkills =HTMLskills.replace("%data%", bio.skills);
-        $("#header").append(formattedSkills);
-    }
-}
+ }
 
 bio.display();
 
@@ -64,19 +64,22 @@ var work = {
         "title": "Sr. Product Marketing Manager",
         "location": "Beijing, China",
         "dates": "2008-2010",
-        "description": "• Completed web traffics data analysis to optimize Web content, functionality, user experience and monetization."},
+        "url": "http://jinku.com",
+        "description": "<li>Completed web traffics data analysis to optimize Web content, functionality, user experience and monetization.</li><li>Managed the life cycle of system project, including user requirement, development, testing, implementation and maintenance support. </li><li>Led a team to initiate the “Certificate Financial Planner Member Communication” magazine. </li>"},
     {
         "employer": "SouFun Holdings Limited ",
         "title": "Web Content Manager",
         "location": "Beijing, China",
-        "dates": "2008-2010",
-        "description": "• Completed web traffics data analysis to optimize Web content, functionality, user experience and monetization."},
+        "dates": "2006-2008",
+        "url": "http://bj.fang.com",
+        "description": "<li>Analyzed real-estate industry information to provide usable sources to digital users.</li><li>Launched coverage, press on websites and social networks.</li><li>Managed the daily website updates.</li>"},
     {
         "employer": "Beijing Sina Co. Ltd.",
         "title": "product marketing manager",
         "location": "Beijing, China",
         "dates": "2004-2006",
-        "description": "Twitter makes it easy to connect with experts in your field. Take advantage of your connections by re-tweeting and commenting on their tweets."}]
+        "url": "http://www.sina.com.cn",
+        "description": "<li>Conducted local business database project. My responsibilities include: gathering requirements, defining minimal viable product, conducting UX usability study, designing database schemas and assisting partners to gather the local businesses data into the databases.</li><li>Utilized clickstream analysis and root cause analysis to identify performance metrics, converted digital data into business strategies, and executed strategic plans to improve website performance.</li><li>Served as the central liaison of internal business units to target new market segment and expand existing AD business partners base.</li>"}]
 }
 
 /* This function is used to update work section to the resume */
@@ -85,6 +88,7 @@ function displayWork() {
         $("#workExperience").append(HTMLworkStart);
 
         var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+        var formattedEmployer = formattedEmployer.replace("#", work.jobs[job].url)
         var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
         var formattedEmployerTitle = formattedEmployer + formattedTitle;
         $(".work-entry:last").append(formattedEmployerTitle);
@@ -105,17 +109,10 @@ displayWork();
 var education = {
     "schools": [
     {
-        "name": "Udacity",
-        "location": "San Francisco, CA",
-        "degree": "Nano Degree",
-        "majors": ["Front-End Development"],
-        "dates": "September 2015 - Present",
-        "url": "https://www.udacity.com"},
-    {
         "name": "University of Nebraska - Lincoln",
         "location": "Lincoln, NE",
         "degree": "Master",
-        "majors": ["MBA"],
+        "majors": ["MBA Specialized in Management Information Systems (MIS)"],
         "dates": "August 2012 - May 2015",
         "url": "http://www.unl.edu"},
     {
@@ -127,16 +124,10 @@ var education = {
         "url": "http://www.bwu.edu.cn"}],
     "onlineCourses": [
     {
-        "title": "Intro to HTML and CSS",
+        "title": "Front-End Web Developer Nanodegree",
         "school": "Udacity",
-        "dates": "2015",
-        "url": "https://www.udacity.com/course/intro-to-html-and-css--ud304-nd"
-    },
-    {
-        "title": "Responsive Web Design Fundamentals",
-        "school": "Udacity",
-        "dates": "2015",
-        "url": "https://www.udacity.com/course/responsive-web-design-fundamentals--ud893-nd"
+        "dates": "September 2015 - Present",
+        "url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001",
     }]
 }
 
@@ -181,35 +172,66 @@ function displayEducation() {
         var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[course].dates);
         $(".education-entry:last").append(formattedOnlineDates);
 
-        var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[course].url);
-        var formattedOnlineURL = formattedOnlineURL.replace("#", education.onlineCourses[course].url);
-        $(".education-entry:last").append(formattedOnlineURL);
+        // var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[course].url);
+        // var formattedOnlineURL = formattedOnlineURL.replace("#", education.onlineCourses[course].url);
+        // $(".education-entry:last").append(formattedOnlineURL);
     }
 }
 
 displayEducation();
 
 var projects = {
-    "projects" :[
-    {
-        "title" : "Portfolio",
-        "dates" : "2016",
-        "description": "In portfolio responsive website project I replicated a design mockup in HTML and CSS, and added a carousel and modals to personalize the page design. ",
-        "images":["images/project.jpg"]},
-    {
-        "title" : "Resume",
-        "dates" : "2016",
-        "description": "I completed a resum project, which is build with JavaScript script.",
-        "images": ["images/project.jpg"]}
-    ]
+    "projects": [{
+        "title": "Portfolio Site",
+        "dates": "January 2016",
+        "description": "This portfolio responsive website is my first Udacity Front-End Web Developer Nanodegree project. In this project I replicated a design mockup in HTML and CSS, and added a carousel and modals to personalize the page design. The techniques that I used include: <li>Bootstrap gribs system and Bootstrap carousel</li><li>Responsive image with Bootstrap's img-responsive calss，picture element, source element and srcset attribute</li><li>Typography responsive</li><li>Grunt to optimize images</li>",
+        "width": "width:100%",
+        "progress": "100% Complete",
+        "url": "https://github.com/wangyi0101/udacity-portfolio"
+    }, {
+        "title": "Interactive Resume",
+        "dates": "February 2016",
+        "description": "This is my second Udacity Front-End Web Developer Nanodegree project. In this project I develop an interactive resume application, which reads resume content from a JSON file and dynamically display that content. Other than using a provided template, I personalized my resume via bootstrap and D3.js.",
+        "width": "width:100%",
+        "progress": "100% Complete",
+        "url": "https://github.com/wangyi0101/frontend-nanodegree-resume"
+    }, {
+        "title": "Classic Arcade Game Clone",
+        "dates": "2016",
+        "description": "You will be provided with visual assets and a game loop engine; using these tools you must add a number of entities to the game including the player characters and enemies to recreate the classic arcade game Frogger.",
+        "width": "width:10%",
+        "progress": "10% Complete",
+        "url": "#"
+    }, {
+        "title": "Website Optimization",
+        "dates": "2016",
+        "description": "You will optimize a provided website with a number of optimization- and performance-related issues so that it achieves a target PageSpeed score and runs at 60 frames per second.",
+        "width": "width:5%",
+        "progress": "5% Complete",
+        "url": "#"
+    }, {
+        "title": "Neighborhood Map",
+        "dates": "2016",
+        "description": "You will develop a single-page application featuring a map of your neighborhood or a neighborhood you would like to visit. You will then add additional functionality to this application, including: map markers to identify popular locations or places you’d like to visit, a search function to easily discover these locations, and a listview to support simple browsing of all locations. You will then research and implement third-party APIs that provide additional information about each of these locations (such as StreetView images, Wikipedia articles, Yelp reviews, etc).",
+        "width": "width:3%",
+        "progress": "3% Complete",
+        "url": "#"
+    }, {
+        "title": "Feed Reader Testing",
+        "dates": "2016",
+        "description": "In this project you are given a web-based application that reads RSS feeds. The original developer of this application clearly saw the value in testing, they’ve already included Jasmine and even started writing their first test suite! Unfortunately, they decided to move on to start their own company and we’re now left with an application with an incomplete test suite. That’s where you come in.",
+        "width": "width:3%",
+        "progress": "3% Complete",
+        "url": "#"
+    }]
 }
-
 /* This function is used to update project section. */
 projects.display = function() {
     for (project in projects.projects){
         $("#projects").append(HTMLprojectStart);
 
-        var formattedProjectTitle= HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+        var formattedProjectTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+        var formattedProjectTitle = formattedProjectTitle.replace("#", projects.projects[project].url);
         $(".project-entry:last").append(formattedProjectTitle);
 
         var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
@@ -218,12 +240,16 @@ projects.display = function() {
         var formattedProjectDes = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
         $(".project-entry:last").append(formattedProjectDes);
 
-        if(projects.projects[project].images.length > 0) {
-            for (image in projects.projects[project].images) {
-                var formattedProjectImg = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
-                $(".project-entry:last").append(formattedProjectImg);
-            }
-        }
+        // if(projects.projects[project].images.length > 0) {
+        //     for (image in projects.projects[project].images) {
+        //         var formattedProjectImg = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+        //         $(".project-entry:last").append(formattedProjectImg);
+        //     }
+        // }
+
+        var formattedProjectProgress = HTMLprojectProgress.replace("#", projects.projects[project].width);
+        var formattedProjectProgress = formattedProjectProgress.replace("%data%", projects.projects[project].progress);
+        $(".project-entry:last").append(formattedProjectProgress);
     }
 }
 
